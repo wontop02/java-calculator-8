33ed62fbe2cece7709cf8ec6c5704075b4dae593
wontop02 <wjy0502kr@naver.com>
2025-10-17 02:36:27 +0900

    feat: 사용자 입력값을 1차 검증하는 BasicValidator 구현

---
+++ src/main/java/calculator/validator/BasicValidator.java
@@ -0,0 +1,17 @@
+package calculator.validator;
+
+// 입력값 1차 검증
+public class BasicValidator {
+    public void validate(String input) {
+        if (input.matches("^//.*")) {
+            if (!input.contains("\\n"))
+                throw new IllegalArgumentException("커스텀 구분자의 마무리가 존재하지 않습니다.");
+            return;
+        }
+        if (input.matches("^\\d.*")) {
+            return;
+        }
+        if (!input.isEmpty())
+            throw new IllegalArgumentException("문자열이 숫자나 커스텀 구분자 지정 문자로 시작하지 않습니다.");
+    }
+}
