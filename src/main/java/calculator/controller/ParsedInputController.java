a0e8aeb1f730c17b8f25a0bbf36b332407d451c7
wontop02 <wjy0502kr@naver.com>
2025-10-18 15:28:32 +0900

    feat: 구분자에 따라 문자열에서 숫자를 추출하는 기능 구현

--- src/main/java/calculator/controller/ParsedInputController.java
+++ src/main/java/calculator/controller/ParsedInputController.java
@@ -1,6 +1,11 @@
 package calculator.controller;
 
 import calculator.validator.CustomDelimiterValidator;
+import calculator.validator.NegativeValidator;
+import calculator.validator.OtherCharacterValidator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class ParsedInputController {
     CustomDelimiterValidator cdv = new CustomDelimiterValidator();
@@ -10,4 +15,26 @@ public class ParsedInputController {
         cdv.validate(delimiter);
         return delimiter;
     }
+
+    public String[] splitString(String input, String delimiter) {
+        if (delimiter.isEmpty())
+            return input.split(",|:");
+        input = input.substring(input.indexOf("\\n") + 2);
+        String regex = Pattern.quote(delimiter);
+        return input.split(regex);
+    }
+
+    public List<Integer> extractNumbers(String[] strNumbers) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String strNum : strNumbers) {
+            if (strNum.isEmpty()) {
+                numbers.add(0);
+                continue;
+            }
+            int num = Integer.parseInt(strNum);
+            numbers.add(num);
+        }
+        return numbers;
+    }
+
 }
