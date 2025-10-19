6fea92edc9c9704f18ff933c31200f1b12c53845
wontop02 <wjy0502kr@naver.com>
2025-10-19 17:29:41 +0900

    refactor: ParsedInput의 result 필드 제거 및 지역변수로 대체

--- src/main/java/calculator/domain/ParsedInput.java
+++ src/main/java/calculator/domain/ParsedInput.java
@@ -3,15 +3,14 @@ package calculator.domain;
 import java.util.List;
 
 public class ParsedInput {
-    private List<Integer> numbers;
-    private int result;
+    private final List<Integer> numbers;
 
     public ParsedInput(List<Integer> numbers) {
         this.numbers = numbers;
     }
 
     public int additionNumbers() {
-        this.result = 0;
+        int result = 0;
         for (int num : numbers)
            result += num;
         return result;
